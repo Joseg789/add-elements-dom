eb373f846cbf3118b9c1a3083232ecbf7f4ac3ba
joseg789 <joseg191.js@gmail.com>
2025-11-02 01:19:51 +0100

    añadiendo archivos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
- // Aquí tu código
+// Aquí tu código
+const handleClick = () => {
+  const element = prompt("Escribe lo que desees agregar a la lista:");
+  if (element) {
+    //creamos el elemento li y lo agregamos a la lista
+    const newItem = document.createElement("li");
+    newItem.textContent = element; //cambiamnos el texto del li
+    list.appendChild(newItem); //agregamos el li a la lista
+  } else {
+    // Si no se ingresó ningún elemento, mostramos un mensaje de alerta
+    alert("No se agregó ningún elemento.");
+  }
+};
+// Selección de elementos y asignación de eventos
+const list = document.getElementById("lista");
+const btn = document.getElementById("agregar");
+// Asignamos el evento click al botón
+btn.addEventListener("click", handleClick); //le pasamos la función que se ejecutará al hacer click
